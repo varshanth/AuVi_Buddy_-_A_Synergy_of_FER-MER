8545daa9b3fcedb8acaaf7a3d9ea8032a23784bd
Igor Braga <higorb1@gmail.com>
2018-03-25 00:23:17 -0400

    Updated valence arousal mapping

--- FER_MER_data_set_utils.py
+++ FER_MER_data_set_utils.py
@@ -480,18 +480,17 @@ def convert_arousal_valence_to_emotion(arousal, valence):
        or valence > _pos_limit):
         return _emotion_label[emotion]
     
-    if(valence > 0.5):
+    if(valence > 0.35):
         emotion = 2
-    elif(valence > 0.26):
-        if(arousal > -0.25): emotion = 2
+    elif(valence > 0.23):
+        if(arousal > 0.23 or arousal < 0.0): emotion = 2
         else: emotion = 4
-    elif(valence > -0.4):
-        if(arousal > 0.5): emotion = 1
-        elif(arousal > 0.18): emotion = 0
-        elif(arousal > -0.22): emotion = 3
-        else: emotion = 4
+    elif(valence > 0.1):
+        if(arousal > 0.1 and arousal < 0.3): emotion = 0
+        elif(arousal >= 0.3): emotion = 1
+        else: emotion = 3
     else:
-        if(arousal > 0.2): emotion = 0
+        if(arousal > 0.18): emotion = 0
         else: emotion = 3
     return _emotion_label[emotion]
 
